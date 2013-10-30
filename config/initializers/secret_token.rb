72650ee155d114b5ad00941cb9d73ac19c3fbd45
Apprentice <apprentice@dbc10.local>
2013-10-30 10:42:55 -0700

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wsse::Application.config.secret_token = '56f48e947b644229eb1b41e2f558b8b00535a240f93d640b9660d2ec594055abe02c56ada6493c84e76c2b5ca773fd244241b92ca59712347e98ef6de1d4283e'
