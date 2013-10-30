f9ea39dae489f9ec64063ae8694843f2b556d39a
Apprentice <apprentice@dbc10.local>
2013-10-30 11:17:03 -0700

    add index action to restaurants controller

--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -8,5 +8,13 @@ describe RestaurantsController do
       response.status.should eq(200)
     end
 
+    it "assigns @restaurants" do
+      restaurants = Restaurant.all
+      get :index
+      expect(assigns(:restaurants)).to eq(restaurants)
+    end
+
+
+
   end
 end
