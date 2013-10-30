dc43ba171708fb40ef223b8e6ac27bf46ee8672e
Apprentice <apprentice@dbc10.local>
2013-10-30 15:55:31 -0700

    add validate presence of content for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
 
   belongs_to :restaurant
-
+  validates_presence_of :content
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 describe Comment do
   it { should belong_to :restaurant }
-
+  it { should validate_presence_of :content }
 end
