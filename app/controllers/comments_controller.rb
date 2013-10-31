b4e2739415aef1097494e900426eb5ed76acd699
Myles <myles@myles.id.au>
2013-10-31 16:24:30 -0700

    passed comment tests, wrote failing vote test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.new(params[:comment])
-    comment.restaurant_id = params[:restaurant_id]
+        p params
+    comment = Comment.new(params[:post])
     comment.save
     comment_html = render_to_string partial: "comments/comment", layout: false, locals: { comment: comment }
     render json: { html: comment_html }.to_json
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,18 @@
 FactoryGirl.define do
   factory :restaurant do
     name 'Nopa'
+    user_id 1
   end
 
   factory :comment do
     content 'get the burger. seriously. so speaketh jeff'
+    user_id 1
+    restaurant_id 1
+  end
+
+  factory :user do
+    username 'boot'
+    email 'boot@dbc.com'
+    password 'password123'
   end
 end
